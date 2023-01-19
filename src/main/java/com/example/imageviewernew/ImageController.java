3980115a8baaedaac269cee4cf06005bd5807714
etg17 <etg1709@gmail.com>
2023-01-19 15:03:24 +0000

    blueScale and greenScale methods working

--- src/main/java/com/example/imageviewernew/ImageController.java
+++ src/main/java/com/example/imageviewernew/ImageController.java
@@ -2,7 +2,6 @@ package com.example.imageviewernew;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -28,6 +27,8 @@ public class ImageController {
     public Label iSize;
     public Label iWidth;
     public Label iHeight;
+    public MenuItem makeBlue;
+    public MenuItem makeGreen;
     @FXML
     private Label welcomeText;
 
@@ -51,17 +52,17 @@ public class ImageController {
 
         iName.setText(file.getName());
         iSize.setText(file.length() + "KB");
-        iWidth.setText(String.valueOf(originalImage.getImage().getWidth()+ "PX"));
+        iWidth.setText(String.valueOf(originalImage.getImage().getWidth() + "PX"));
         iHeight.setText(String.valueOf(originalImage.getImage().getHeight() + "PX"));
     }
 
     public void turnPictureGrey(ActionEvent actionEvent) {
         Image image = originalImage.getImage();
 
-        if (image == null){
+        if (image == null) {
             System.out.println("No image selected");
             return;
-    }
+        }
         PixelReader pixelReader = image.getPixelReader();
 
         int width = (int) image.getWidth();
@@ -81,7 +82,7 @@ public class ImageController {
                 int grayLevel = (int) (0.2162 * red + 0.7152 * green + 0.0722 * blue);
                 int grey = (alpha << 24) + (grayLevel << 16) + (grayLevel << 8) + grayLevel;
 
-                grayImage.getPixelWriter().setArgb( x, y, grey);
+                grayImage.getPixelWriter().setArgb(x, y, grey);
             }
 
             alteredImage.setImage(grayImage);
@@ -92,7 +93,7 @@ public class ImageController {
     public void turnRedScale(ActionEvent actionEvent) {
         Image image = originalImage.getImage();
 
-        if (image == null){
+        if (image == null) {
             System.out.println("No image selected");
             return;
         }
@@ -111,12 +112,80 @@ public class ImageController {
                 int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
 
-
-
                 pixel = (alpha << 24) | (red << 16) | (0 << 8);
+                redImage.getPixelWriter().setArgb(x, y, pixel);
+            }
+
+            alteredImage.setImage(redImage);
+
+        }
+    }
+
+    public void turnBlueScale(ActionEvent actionEvent) {
+
+        Image image = originalImage.getImage();
+
+        if (image == null) {
+            System.out.println("No image selected");
+            return;
+        }
+
+        PixelReader pixelReader = image.getPixelReader();
+
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        WritableImage redImage = new WritableImage(width, height);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = pixelReader.getArgb(x, y);
+
+                int alpha = ((pixel >> 24) & 0xff);
+                int blue = (pixel & 0xff);
 
 
-                redImage.getPixelWriter().setArgb(x,y,pixel);
+                pixel = (alpha << 24) | (0 << 16) | (0 << 8)| blue;
+
+
+                redImage.getPixelWriter().setArgb(x, y, pixel);
+
+
+            }
+
+            alteredImage.setImage(redImage);
+
+        }
+    }
+
+    public void turnGreenScale(ActionEvent actionEvent) {
+
+        Image image = originalImage.getImage();
+
+        if (image == null) {
+            System.out.println("No image selected");
+            return;
+        }
+
+        PixelReader pixelReader = image.getPixelReader();
+
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        WritableImage redImage = new WritableImage(width, height);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = pixelReader.getArgb(x, y);
+
+                int alpha = ((pixel >> 24) & 0xff);
+                int green = (pixel & 0xff);
+
+
+                pixel = (alpha << 24) | (0 << 16) | (green << 8);
+
+
+                redImage.getPixelWriter().setArgb(x, y, pixel);
 
 
             }
@@ -126,3 +195,4 @@ public class ImageController {
         }
     }
 }
+
